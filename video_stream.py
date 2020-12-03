2b0dc891bd05ac3162e4c69ac93eb15ec00ea733
Christina Bukas <31160776+christinab12@users.noreply.github.com>
2020-12-03 15:41:35 +0100

    Add files via upload

---
+++ video_stream.py
@@ -0,0 +1,82 @@
+
+import os
+import tensorflow as tf
+import tensorflow_hub as hub
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+import IPython.display as display
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12,12)
+mpl.rcParams['axes.grid'] = False
+import numpy as np
+import PIL.Image
+import time
+import functools
+import numpy as np
+import cv2
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def prepare_img(img):
+  max_dim = 512
+  img = tf.convert_to_tensor(img)
+  #img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return tensor #PIL.Image.fromarray(tensor)
+
+hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+print('loaded model')
+style_path = '/Users/christina.bukas/Downloads/trippy-cells-biology-green-wallpaper-thumb.jpg'
+style_image = load_img(style_path)
+
+cap = cv2.VideoCapture(0)
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    content_image = prepare_img(frame)
+    stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+    new_img = tensor_to_image(stylized_image)
+
+    # Display the resulting frame
+    cv2.imshow('Style Transfer',new_img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
