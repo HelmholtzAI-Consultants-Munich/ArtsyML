c43eb9e7218cb7abd8a71267a57ff577ba1b5c8e
ruolin shen <ruolins.shen@gmail.com>
2021-06-18 10:24:43 +0200

    two tf model

---
+++ test.py
@@ -0,0 +1,117 @@
+import os
+import argparse
+import cv2 # used for resize. if you dont have it, use anything else
+import numpy as np
+from model import Deeplabv3
+import tensorflow as tf
+import tensorflow_hub as tfhub
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+import IPython.display as display
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12,12)
+mpl.rcParams['axes.grid'] = False
+import functools
+import time
+
+def load_img(path_to_img):
+    max_dim = 512
+    img = tf.io.read_file(path_to_img)
+    img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def prepare_img(img):
+    max_dim = 512
+    img = tf.convert_to_tensor(img)
+    #img = tf.image.decode_image(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+def tensor_to_image(tensor):
+    tensor = tensor*255
+    tensor = np.array(tensor, dtype=np.uint8)
+    if np.ndim(tensor)>3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return tensor #PIL.Image.fromarray(tensor)
+
+def get_args():
+    parser = argparse.ArgumentParser(description='ArtsyML')
+    parser.add_argument('--style_img', default='./images_style/style3.jpg')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+
+    args = get_args()
+    style_path = args.style_img
+    
+    style_image = load_img(style_path)
+    style_image = tf.stack([style_image[:,:,:,2],style_image[:,:,:,1],style_image[:,:,:,0]],axis = 3)
+    style_model = tfhub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+#     print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
+    seg_model = Deeplabv3(backbone='mobilenetv2',input_shape=(288,512,3), OS=16)
+
+    print('loaded model')
+    cap = cv2.VideoCapture(0)
+    fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+    videoWriter = cv2.VideoWriter('video.avi', fourcc, 30.0, (288,512))
+    prev_capture = time.time()
+    while(10):
+        
+        capture_time = time.time()
+        print('Time between captures: ', capture_time - prev_capture)
+        prev_capture = capture_time
+
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Preparing the frame for the style net
+        content_image = prepare_img(frame)
+        style_image_tensor = style_model(tf.constant(content_image), tf.constant(style_image))[0]
+        style_img = tensor_to_image(style_image_tensor)
+
+        # Preparing the frame for the segmentation net 
+        frame = cv2.resize(frame, (style_img.shape[1], style_img.shape[0]))
+        resized = frame / 127.5 - 1.
+        res = seg_model.predict(np.expand_dims(resized,0))
+        seg_mask = np.argmax(res.squeeze(),-1).astype(np.uint8)
+        seg_mask[seg_mask==15] = 1
+    
+
+        # keep people only from style image and background only from original frame
+        style_img =  (1-seg_mask[:,:,None])*frame + seg_mask[:,:,None]*style_img
+        style_img = style_img.astype(np.uint8)
+
+        # Display the resulting frame
+        cv2.imshow('Style Transfer', style_img)
+        videoWriter.write(style_img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+
