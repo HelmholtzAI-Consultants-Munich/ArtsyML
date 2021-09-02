b64ae3f9ae0d39159cc6d7af79ae15e53c83e543
Christina Bukas <ga27loj@mytum.de>
2021-09-02 15:16:12 +0200

    adjusted style1 and speed-up on test server

--- test_server.py
+++ test_server.py
@@ -6,12 +6,6 @@ import tensorflow as tf
 import tensorflow_hub as tfhub
 # Load compressed models from tensorflow_hub
 os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
-import IPython.display as display
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-mpl.rcParams['figure.figsize'] = (12,12)
-mpl.rcParams['axes.grid'] = False
-import functools
 import time
 from model import Deeplabv3
 
@@ -55,8 +49,8 @@ def load_img(path_to_img, max_dim=512): #1024):
 
 def combine(content_image,style_image,seg_content_image):
     style_image_tensor = style_model(content_image, style_image)[0]
-    res = seg_model(seg_content_image) #**{'input.1': seg_content_image})[0]
-    return style_image_tensor,res
+    segmetation_mask = seg_model(seg_content_image)
+    return style_image_tensor,segmetation_mask
     
 
 def get_args():
@@ -69,9 +63,7 @@ if __name__ == '__main__':
 
     args = get_args()
  
-    style_path = args.style_img
-    print(style_path)
-    
+    style_path = args.style_img    
     style_image = load_img(style_path)
     
     style_image = tf.stack([style_image[:,:,:,2],style_image[:,:,:,1],style_image[:,:,:,0]],axis = 3)
@@ -84,7 +76,8 @@ if __name__ == '__main__':
     print('model loaded')
     
     # load video to simulate cam reading
-    arr = np.load('video.npy') 
+    arr = np.load('./input_videos/long_moving_720p.npy') 
+    
     frame_dim1, frame_dim2 = (arr[0].shape[0], arr[0].shape[1])  #(360, 640) #(720, 1280) #(1080, 1920)
     
     first_time = time.time()
@@ -97,70 +90,56 @@ if __name__ == '__main__':
         if i>1:
             avg_frame_rate += time.time() - prev_capture 
         prev_capture = time.time()
-        
+
         # Capture frame-by-frame
         frame = arr[i].astype(np.uint8)
-        
+        frame_t = tf.cast(tf.convert_to_tensor(frame), tf.float32)
+
+
         frame_resized = cv2.resize(frame, (seg_sizes[1], seg_sizes[0]))
         seg_content_image = np.expand_dims(frame_resized / 127.5 - 1.,0)
-        
+
         # Preparing the frame for the style net
-        content_image = tf.convert_to_tensor(frame_resized)
-        content_image = prepare_img_style(content_image) #, frame_dim2)
-        '''
-        if i==0:
-            print('SIZES', content_image.shape, style_image.shape, seg_content_image.shape)
-        '''
+        frame_tensor = tf.convert_to_tensor(frame_resized)
+        content_image = prepare_img_style(frame_tensor) #, frame_dim2)
+
         tik = time.time()
-        print('preprocessing time: ',tik-prev_capture)
+        #print('preprocessing time: ',tik-prev_capture)
 
         style_image_tensor,seg_image_tensor = tf_combine_model(tf.constant(content_image),tf.constant(style_image),tf.constant(seg_content_image))
-        
+
         tok = time.time()
         print('inference time: ',tok-tik)
-        
-        style_image_tensor = tf.image.resize(style_image_tensor, (frame.shape[0], frame.shape[1]))  
-#         stylized_img = tensor_to_image(style_image_tensor)
-        style_image_tensor = tf.squeeze(style_image_tensor, [0])*255
 
+        style_image_tensor = tf.image.resize(style_image_tensor, (frame.shape[0], frame.shape[1])) 
+        style_image_tensor = tf.squeeze(style_image_tensor, [0])*255 
         seg_image_tensor = tf.argmax(seg_image_tensor, axis=3)
-        seg_mask = tf.math.equal(seg_image_tensor,15)        
-        seg_mask = tf.dtypes.cast(tf.stack([seg_mask] * 3, axis=-1),tf.float32)
-        
-        seg_mask = tf.image.resize(seg_mask, (frame.shape[0], frame.shape[1]))        
-        seg_mask = tf.squeeze(seg_mask, [0])
+        seg_mask = tf.math.equal(seg_image_tensor,15) 
+        seg_mask = tf.dtypes.cast(tf.stack([seg_mask] * 3, axis=-1), tf.float32)
+        seg_mask = tf.image.resize(seg_mask, (frame.shape[0], frame.shape[1]))     
+        seg_mask = tf.squeeze(seg_mask[0])
 
-#         seg_mask = np.array(seg_image_tensor[0,:,:]) #, dtype=np.uint8)        
-#         seg_mask[seg_mask!=15] = 0
-#         seg_mask[seg_mask==15] = 1        
-#         seg_mask = cv2.resize(seg_mask.astype(np.uint8), (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_NEAREST)
-#         seg_mask = seg_mask[:,:,None].astype(np.uint8)
-        
-        # keep people only from style image and background only from original frame
-#         stylized_img =  (1-seg_mask)*frame + seg_mask*stylized_img
-        stylized_img = seg_mask*style_image_tensor+(1-seg_mask)*frame
-        stylized_img = np.array(stylized_img)
-    
+        #stylized_img = seg_mask*style_image_tensor+(1-seg_mask)*frame
+        stylized_img = seg_mask*style_image_tensor+(1-seg_mask)*tf.cast(tf.convert_to_tensor(frame), tf.float32)
+        stylized_img = stylized_img.numpy() #tf.make_ndarray(stylized_img) # np.array(stylized_img)
+        list_res.append(stylized_img.astype(np.uint8))
         tik = time.time()
         print('postprocessing time: ',tik-tok)
         print(' ')
         
-        #stylized_img = cv2.resize(stylized_img, (frame_dim2, frame_dim1))
-        list_res.append(stylized_img)
 
     fps = round((arr.shape[0]-2)/avg_frame_rate)
     print('Average SPF: ', avg_frame_rate/(arr.shape[0]-2),'and FPS: ', fps)
     
     # save result as video
-#     style_name = style_path.split('/')[-1].split('.')[0]
-#     output_name = ('_').join([style_name, str(frame_dim1)+'p', str(fps)+'fps.mp4'])
-    output_path = 'output_videos.mp4'
+    style_name = style_path.split('/')[-1].split('.')[0]
+    output_name = ('_').join([style_name, str(frame_dim1)+'p', str(fps)+'fps.mp4'])
     
     if fps==16: # for some reason i get an error when saving video with 16fps
         print('OhOh')
         fps=17 
-        
-    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_dim2, frame_dim1)) 
+
+    out = cv2.VideoWriter(output_name, cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_dim2, frame_dim1)) 
     for stylized_img in list_res:
-        out.write(stylized_img.astype(np.uint8))
+        out.write(stylized_img)
     out.release()
