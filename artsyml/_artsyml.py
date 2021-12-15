f543a4bd8d40c1e01396783ed501be217ed84bef
christinab12 <christina.bukas@helmholtz-muenchen.de>
2021-12-15 15:32:33 +0100

    changed to dynamic mem alloc

--- artsyml/_artsyml.py
+++ artsyml/_artsyml.py
@@ -6,6 +6,9 @@ import os
 import cv2
 import numpy as np
 
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
 class ArtsyML():
     def __init__(self, style_image_file, frame_shape = (720, 1280), seg_shape = (288,512,3)):
         self.frame_shape = frame_shape
